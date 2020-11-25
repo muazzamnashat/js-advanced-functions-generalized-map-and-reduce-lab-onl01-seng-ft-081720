1b6e29a25902634369ebb2d1eadaabaa94b9c228
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-11-25 03:24:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, func){
+    let result=[]
+    array.forEach(el => {
+        result.push(func(el))
+    })
+    return result
+}
+
+function reduce(array,func,start){
+    let initial = start? start : 0
+    let status;
+    for (let i = 0; i < array.length; i+=2){
+        let currEl = array[i]
+        let first = array[i] ? array[i]: 0
+        let second = array[i+1] ? array[i+1]:0
+        if (typeof currEl !== "number"){
+          status = func(first,second)
+        } 
+        initial += func(first,second)
+    }
+    if (status === true || status === false) return status;
+    return initial
+}
+
